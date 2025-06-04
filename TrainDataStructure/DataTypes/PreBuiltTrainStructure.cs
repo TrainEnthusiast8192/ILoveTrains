29d5b4abd1f3e4d6bb40d33142608b62adc07fef
TrainEnthusiast8192 <p8192290@gmail.com>
2025-06-04 23:07:08 +0200

    Added even more constructors for prebuilts

--- TrainDataStructure/DataTypes/PreBuiltTrainStructure.cs
+++ TrainDataStructure/DataTypes/PreBuiltTrainStructure.cs
@@ -43,6 +43,42 @@ public record class PreBuiltTrainStructure
             }
         }
     }
+    public PreBuiltTrainStructure(PreBuiltTrainStructureEvents onConnectionSuccess, PreBuiltTrainStructureEvents onConnectionFailure, params AbstractTrainNode[] nodes)
+    {
+        OnConnectionSuccess = onConnectionSuccess;
+        OnConnectionFailure = onConnectionFailure;
+
+        Count = 0;
+        IsLinear = true;
+        IsTypeSafe = true;
+        Type? firstSeenType = null;
+
+        if (nodes.Length == 0) { throw new ArgumentException("No nodes supplied to constructor of prebuilt"); }
+        AbstractTrainNode firstInNodes = nodes[0];
+        first = firstInNodes;
+        firstInNodes.ReParent(null);
+
+        int cnt = nodes.Length;
+        for (int i = 0; i < cnt; i++)
+        {
+            AbstractTrainNode n = nodes[i];
+            if (first.AddNode(n, new()).Is(TrainOperations.SUCCESS))
+            {
+                Count++;
+                if (n.IsForking)
+                {
+                    IsLinear = false;
+                }
+                if (n.IsValueNode)
+                {
+                    ValueTrainNode<IComparable> vnode = (ValueTrainNode<IComparable>)n;
+                    Type? T = vnode.GetValue()?.GetType();
+                    firstSeenType ??= T;
+                    IsTypeSafe &= Type.Equals(T, firstSeenType);
+                }
+            }
+        }
+    }
     public PreBuiltTrainStructure(AbstractTrainNode first, int count)
     {
         this.first = first;
