7f1633d821847821a5c061055b4eacf1b18c3877
TrainEnthusiast8192 <p8192290@gmail.com>
2025-06-06 15:17:37 +0200

    Added TrainExplorer. Now including editor config and solution file

---
+++ TrainExplorer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+
+Train<byte> train = new(new TestNode());
+var s = new TrainSignal([], [], false, true);
+s.OnSignalSuccess += gunc;
+train[0, IndexerInference.NODE]!.ReLink(train[0, IndexerInference.NODE]);
+train.Signal(s);
+
+train.PrintTrain();
+
+string? str = Console.ReadLine();
+
+void gunc(TrainSignal s) { Console.WriteLine("SIG"); }
+
+
+public class TestNode : WaitTrainNode
+{
+    public TestNode() : base(100) { }
+    protected override TrainOperations HandleSignal(TrainSignal signal, Stack<AbstractTrainNode> loopedOver)
+    {
+        if (loopedOver.Contains(this))
+        {
+            return TrainOperations.LOOPED;
+        }
+        if (signal.IsCancelled)
+        {
+            return TrainOperations.CANCELLED;
+        }
+
+        if (signal.IsAwaitable)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(millis);
+                GetNext()?.Signal(signal, []);
+            });
+
+            return TrainOperations.SUCCESS;
+        }
+
+        return TrainOperations.PASS;
+    }
+}
