be87ab5db9d377475c34d0950bc22e0d7643ded5
TrainEnthusiast8192 <p8192290@gmail.com>
2025-06-09 15:59:57 +0200

    Moved to new subnamespace

---
+++ TrainDataStructure/Serialization/History/ITrainHistoryEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainDataStructure.Serialization.History;
+public interface ITrainHistoryEntry
+{
+    public const char SERIALIZATION_SEPARATOR = AbstractTrainNode.SERIALIZATION_SEPARATOR;
+    public abstract ITrainCollection GetTrain();
+    public abstract IEnumerable<ITrainHistoryEntry> GetStoringCollection();
+    public abstract void Execute();
+    public abstract DateTime GetAbsoluteTimeStamp();
+    public abstract int CalculateMillisToDelay(DateTime creationTime);
+    public abstract string Serialize();
+}
---
+++ TrainDataStructure/Serialization/History/TrainNodeAddedHistoryEntry.cs
@@ -0,0 +1,26 @@
+﻿namespace TrainDataStructure.Serialization.History;
+public readonly struct TrainNodeAddedHistoryEntry(ITrainCollection parentTrain, Queue<ITrainHistoryEntry> storingCollection, DateTime timeStamp, AbstractTrainNode node) : ITrainHistoryEntry
+{
+    public const char SERIALIZATION_SEPARATOR = ITrainHistoryEntry.SERIALIZATION_SEPARATOR;
+    private readonly ITrainCollection parentTrain = parentTrain;
+    public readonly ITrainCollection GetTrain() => parentTrain;
+
+    private readonly Queue<ITrainHistoryEntry> storingCollection = storingCollection;
+    public readonly IEnumerable<ITrainHistoryEntry> GetStoringCollection() => storingCollection;
+
+    private readonly DateTime timeStamp = timeStamp;
+    public readonly DateTime GetAbsoluteTimeStamp() => timeStamp;
+    public readonly int CalculateMillisToDelay(DateTime creationTime) => (timeStamp - creationTime).Milliseconds;
+
+    private readonly string node = node.Serialize();
+
+    public readonly string Serialize()
+    {
+        return $"AddNode{SERIALIZATION_SEPARATOR}{parentTrain.GetID()}{SERIALIZATION_SEPARATOR}{timeStamp}{SERIALIZATION_SEPARATOR}{node}";
+    }
+
+    public readonly void Execute()
+    {
+        parentTrain.Add(NodeDeSerializer.DeSerialize(node));
+    }
+}
