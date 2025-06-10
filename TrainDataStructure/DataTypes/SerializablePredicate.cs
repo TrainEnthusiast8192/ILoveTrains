d47e7f7ec89b78e1c0c7bde8fdf50fd427efd0cb
TrainEnthusiast8192 <p8192290@gmail.com>
2025-06-10 22:30:15 +0200

    Added serializable predicates

---
+++ TrainDataStructure/DataTypes/SerializablePredicate.cs
@@ -0,0 +1,32 @@
+﻿namespace TrainDataStructure.DataTypes;
+public sealed class SerializablePredicate<T> : IComparable
+{
+    private readonly Func<T, bool> AsPredicate;
+    private readonly Expression<Func<T, bool>> AsExpression;
+
+    public bool Invoke(T argument) => AsPredicate.Invoke(argument);
+
+    private SerializablePredicate(Func<T, bool> predicate, Expression<Func<T, bool>> expression)
+    {
+        AsPredicate = predicate;
+        AsExpression = expression;
+    }
+    public SerializablePredicate(Expression<Func<T, bool>> expression)
+    {
+        AsPredicate = expression.Compile();
+        AsExpression = expression;
+    }
+
+    public override string ToString()
+    {
+        return AsExpression.ToString();
+    }
+
+    public int CompareTo(object? obj)
+    {
+        return 1;
+    }
+
+    public static implicit operator Func<T, bool>(SerializablePredicate<T> serPred) => serPred.AsPredicate;
+    public static implicit operator Expression<Func<T, bool>>(SerializablePredicate<T> serPred) => serPred.AsExpression;
+}
