0aedc1544e95766bebd772011e5e26dbc454d369
TrainEnthusiast8192 <p8192290@gmail.com>
2025-06-10 14:44:37 +0200

    Split classes and methods

--- TrainDataStructure/Serialization/NodeDeserializer.cs
+++ TrainDataStructure/Serialization/NodeDeserializer.cs
@@ -6,33 +6,71 @@ public sealed class NodeDeSerializer
 {
     public const char SERIALIZATION_SEPARATOR = AbstractTrainNode.SERIALIZATION_SEPARATOR;
 
-    public static readonly Dictionary<string, Func<string, AbstractTrainNode>> DeSerializers = new();
-    private static readonly NodeDeSerializer _instance = new NodeDeSerializer();
+    public readonly FrozenDictionary<string, Func<string, AbstractTrainNode>> DeSerializers;
+    public static readonly NodeDeSerializer Instance = new NodeDeSerializer();
 
     private NodeDeSerializer()
     {
-        FindDeSerializers();
+        DeSerializers = FindDeSerializers().ToFrozenDictionary();
     }
 
     public static AbstractTrainNode DeSerialize(string serializedNode)
     {
         string firstSegment = serializedNode.Split(SERIALIZATION_SEPARATOR)[0];
-        return DeSerializers.TryGetValue(firstSegment, out Func<string, AbstractTrainNode>? value) ? value.Invoke(serializedNode)
+        return Instance.DeSerializers.TryGetValue(firstSegment, out Func<string, AbstractTrainNode>? value) ? value.Invoke(serializedNode)
             : throw new TrainDeSerializationNoDeSerializerFoundException($"No deserializer found for node: {serializedNode}");
     }
 
-    private void FindDeSerializers()
+    private HashSet<Assembly> FindAssemblies()
     {
-        var externalMethods = Assembly.GetEntryAssembly()?
-                                        .GetTypes().SelectMany(x => x.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static))
-                                        .Where(x => x.GetCustomAttribute<TrainNodeDeSerializerAttribute>() is not null);
-        
-        var libMethods = Assembly.GetAssembly(typeof(NodeDeSerializer))?
-                                .GetTypes().SelectMany(x => x.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static))
+        var rootAssembly = Assembly.GetEntryAssembly();
+
+        HashSet<Assembly> visited = [Assembly.GetAssembly(typeof(NodeDeSerializer))];
+        var queue = new Queue<Assembly?>();
+
+        queue.Enqueue(rootAssembly);
+
+        while (queue.Any())
+        {
+            var assembly = queue.Dequeue();
+            if (assembly is null) { continue; }
+            visited.Add(assembly);
+
+            var references = assembly.GetReferencedAssemblies();
+            foreach (var reference in references)
+            {
+                if (!visited.Contains(Assembly.Load(reference.FullName)))
+                {
+                    queue.Enqueue(Assembly.Load(reference));
+                }
+            }
+        }
+
+        return visited;
+    }
+    private IEnumerable<MethodInfo>? FindMethods()
+    {
+        var visited = FindAssemblies();
+
+        IEnumerable<MethodInfo>? libMethods = null;
+
+        foreach (Assembly assemb in visited)
+        {
+            var methods = assemb.GetTypes().SelectMany(x => x.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static))
                                 .Where(x => x.GetCustomAttribute<TrainNodeDeSerializerAttribute>() is not null);
 
-        var methods = libMethods?.Union(externalMethods ?? []) ?? externalMethods;
-        if (methods is null) { return; }
+            libMethods = libMethods?.Union(methods) ?? [];
+        }
+
+        return libMethods;
+    }
+    private Dictionary<string, Func<string, AbstractTrainNode>> FindDeSerializers()
+    {
+        var methods = FindMethods();
+        if (methods is null) { return []; }
+
+        Dictionary<string, Func<string, AbstractTrainNode>> ret = new();
+
 
         foreach (var method in methods)
         {
@@ -52,98 +90,11 @@ public sealed class NodeDeSerializer
             FrozenSet<string> validNodeTypes = attribute.NodeTypes;
             foreach (string node in validNodeTypes)
             {
-                DeSerializers.Add(node, func);
+                ret.Add(node, func);
             }
         }
-    }
 
-
-    [TrainNodeDeSerializer("OrphanTrainNode", "AbstractTrainNode", "ValueTrainNode", "SwitchTrainNode", "WaitTrainNode", "MarkerTrainNode")]
-    public static AbstractTrainNode StandardDeSerialize(string serializedNode)
-    {
-        Span<string> groups = serializedNode.Split(AbstractTrainNode.SERIALIZATION_SEPARATOR);
-        if (groups.Length == 0) { throw new ArgumentException("String contains no separable groups"); }
-
-        switch (groups[0])
-        {
-            default:
-                throw new TrainDeSerializationInvalidNodeTypeException($"Invalid node type {groups[0]}");
-            case "OrphanTrainNode":
-                throw new TrainDeSerializationAbstractNodeTypeException($"Cannot create abstract node type {groups[0]}");
-            case "AbstractTrainNode":
-                throw new TrainDeSerializationAbstractNodeTypeException($"Cannot create abstract node type {groups[0]}");
-            case "ValueTrainNode":
-                // Get the generic type
-                Type typeParameter = Type.GetType(groups[2]) 
-                    ?? throw new ArgumentException($"Unknown type parameter {groups[1]} found");
-
-                // Get the parse method, which must be static and must accept a single string. TryParse is also valid
-                MethodInfo? parseMethod = typeParameter.GetMethod("Parse", BindingFlags.Public | BindingFlags.Static, [typeof(string)]) 
-                    ?? typeParameter.GetMethod("TryParse", BindingFlags.Public | BindingFlags.Static, [typeof(string), typeParameter.MakeByRefType()]);
-
-                // Pass fourth argument with no caller onto parseMethod
-                // We concatenate them in case the value used the same separator
-                var slice = groups.Slice(3);
-
-                if (typeof(AbstractTrainNode).IsAssignableFrom(typeParameter))
-                {
-                    int cnt = slice.Length;
-                    for (int i = 1; i < cnt; i++)
-                    {
-                        slice[i] = SERIALIZATION_SEPARATOR + slice[i];
-                    }
-                    parseMethod = typeof(NodeDeSerializer).GetMethod("DeSerialize", BindingFlags.Public | BindingFlags.Static);
-                }
-
-                string valToParse = String.Concat(slice);
-                object? value = parseMethod?.Invoke(null, [valToParse]) ?? default;
-
-                Type nodeType = typeof(ValueTrainNode<>).MakeGenericType([typeParameter]);
-
-                // Build the node as a non-generic one
-                AbstractTrainNode node = (AbstractTrainNode)(Activator.CreateInstance(nodeType, value) 
-                    ?? throw new TrainDeSerializationUnknownNodeTypeException($"Unknown node type {nodeType} found"));
-
-                // Get the SetValue method and call it with what we have
-                nodeType.GetMethod("SetValue", BindingFlags.Public, [typeParameter])?.Invoke(node, [value]);
-
-                // Take care of the GUID directly
-                nodeType.GetField("INTERNAL_CONNECTIONS_GUID", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(node, Guid.Parse(groups[1]));
-
-                return node;
-
-            case "SwitchTrainNode":
-                // Simple, parsable fields
-                int id = int.Parse(groups[1]);
-                bool fork = bool.Parse(groups[2]);
-
-                SwitchTrainNode ret = new SwitchTrainNode(id);
-                ret.ForkLeft = fork;
-
-                // Take care of the GUID directly
-                typeof(SwitchTrainNode).GetField("INTERNAL_CONNECTIONS_GUID", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(ret, Guid.Parse(groups[3]));
-
-                return ret;
-
-            case "WaitTrainNode":
-                // Simple, parsable fields
-                int millis = int.Parse(groups[1]);
-                WaitTrainNode waitret = new WaitTrainNode(millis);
-
-                // Take care of the GUID directly
-                typeof(WaitTrainNode).GetField("INTERNAL_CONNECTIONS_GUID", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(waitret, Guid.Parse(groups[2]));
-
-                return waitret;
-
-            case "MarkerTrainNode":
-                string message = groups[1];
-                MarkerTrainNode markerret = new MarkerTrainNode(message);
-
-                // Take care of the GUID directly
-                typeof(MarkerTrainNode).GetField("INTERNAL_CONNECTIONS_GUID", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(markerret, Guid.Parse(groups[2]));
-
-                return markerret;
-        }
+        return ret;
     }
 }
 
---
+++ TrainDataStructure/Serialization/StandardDeSerializer.cs
@@ -0,0 +1,104 @@
+﻿using System.Reflection;
+namespace TrainDataStructure.Serialization;
+public static class StandardDeSerializer
+{
+    public const char SERIALIZATION_SEPARATOR = NodeDeSerializer.SERIALIZATION_SEPARATOR;
+
+    [TrainNodeDeSerializer("OrphanTrainNode", "AbstractTrainNode", "ValueTrainNode", "SwitchTrainNode", "WaitTrainNode", "MarkerTrainNode")]
+    public static AbstractTrainNode StandardDeSerialize(string serializedNode)
+    {
+        Span<string> groups = serializedNode.Split(AbstractTrainNode.SERIALIZATION_SEPARATOR);
+        if (groups.Length == 0) { throw new ArgumentException("String contains no separable groups"); }
+
+        switch (groups[0])
+        {
+            default:
+                throw new TrainDeSerializationInvalidNodeTypeException($"Invalid node type {groups[0]}");
+            case "OrphanTrainNode":
+                throw new TrainDeSerializationAbstractNodeTypeException($"Cannot create abstract node type {groups[0]}");
+            case "AbstractTrainNode":
+                throw new TrainDeSerializationAbstractNodeTypeException($"Cannot create abstract node type {groups[0]}");
+            case "ValueTrainNode":
+                return CaseValueNode(groups);
+
+            case "SwitchTrainNode":
+                // Simple, parsable fields
+                int id = int.Parse(groups[1]);
+                bool fork = bool.Parse(groups[2]);
+
+                SwitchTrainNode switchret = new SwitchTrainNode(id);
+                switchret.ForkLeft = fork;
+
+                // Take care of the GUID directly
+                HandleGUID<SwitchTrainNode>(switchret, Guid.Parse(groups[2]));
+
+                return switchret;
+
+            case "WaitTrainNode":
+                // Simple, parsable fields
+                int millis = int.Parse(groups[1]);
+                WaitTrainNode waitret = new WaitTrainNode(millis);
+
+                // Take care of the GUID directly
+                HandleGUID<WaitTrainNode>(waitret, Guid.Parse(groups[2]));
+
+                return waitret;
+
+            case "MarkerTrainNode":
+                string message = groups[1];
+                MarkerTrainNode markerret = new MarkerTrainNode(message);
+
+                // Take care of the GUID directly
+                HandleGUID<MarkerTrainNode>(markerret, Guid.Parse(groups[2]));
+
+                return markerret;
+        }
+    }
+
+    private static void HandleGUID<NodeType>(NodeType instance, Guid guid)
+    {
+        typeof(NodeType).GetField("INTERNAL_CONNECTIONS_GUID", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(instance, guid);
+    }
+
+    private static AbstractTrainNode CaseValueNode(Span<string> groups)
+    {
+        // Get the generic type
+        Type typeParameter = Type.GetType(groups[2])
+            ?? throw new ArgumentException($"Unknown type parameter {groups[1]} found");
+
+        // Get the parse method, which must be static and must accept a single string. TryParse is also valid
+        MethodInfo? parseMethod = typeParameter.GetMethod("Parse", BindingFlags.Public | BindingFlags.Static, [typeof(string)])
+            ?? typeParameter.GetMethod("TryParse", BindingFlags.Public | BindingFlags.Static, [typeof(string), typeParameter.MakeByRefType()]);
+
+        // Pass fourth argument with no caller onto parseMethod
+        // We concatenate them in case the value used the same separator
+        var slice = groups.Slice(3);
+
+        if (typeof(AbstractTrainNode).IsAssignableFrom(typeParameter))
+        {
+            int cnt = slice.Length;
+            for (int i = 1; i < cnt; i++)
+            {
+                slice[i] = SERIALIZATION_SEPARATOR + slice[i];
+            }
+            parseMethod = typeof(NodeDeSerializer).GetMethod("DeSerialize", BindingFlags.Public | BindingFlags.Static);
+        }
+
+        string valToParse = String.Concat(slice);
+        object? value = parseMethod?.Invoke(null, [valToParse]) ?? default;
+
+        Type nodeType = typeof(ValueTrainNode<>).MakeGenericType([typeParameter]);
+
+        // Build the node as a non-generic one
+        AbstractTrainNode node = (AbstractTrainNode)(Activator.CreateInstance(nodeType, value)
+            ?? throw new TrainDeSerializationUnknownNodeTypeException($"Unknown node type {nodeType} found"));
+
+        // Get the SetValue method and call it with what we have
+        nodeType.GetMethod("SetValue", BindingFlags.Public, [typeParameter])?.Invoke(node, [value]);
+
+        // Take care of the GUID directly
+        nodeType.GetField("INTERNAL_CONNECTIONS_GUID", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(node, Guid.Parse(groups[1]));
+
+        return node;
+    }
+}
