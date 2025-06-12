6e80051b2bad3c36540c4ad8662f70dd47d2aa11
TrainEnthusiast8192 <p8192290@gmail.com>
2025-06-13 00:45:23 +0200

    Stores references to deserialize using compiler services

--- TrainDataStructure/DataTypes/SerializablePredicate.cs
+++ TrainDataStructure/DataTypes/SerializablePredicate.cs
@@ -1,40 +1,80 @@
-﻿using System.Linq.Dynamic.Core;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Text;
 namespace TrainDataStructure.DataTypes;
 public sealed class SerializablePredicate<T> : IComparable
 {
-    private readonly Func<T, bool> AsPredicate;
-    private readonly Expression<Func<T, bool>> AsExpression;
-    private static readonly ParsingConfig CONFIG = ParsingConfig.Default;
+    public const char SERIALIZATION_SEPARATOR = '\u4000';
+    public const char SERIALIZATION_SECTION_SEPARATOR = '\u4001';
+    public readonly Func<T, bool> AsPredicate;
+    private readonly string AsText;
+    private readonly FrozenSet<string> namespaces;
+    private readonly FrozenSet<Assembly> references;
 
-    public bool Invoke(T argument) => AsPredicate.Invoke(argument);
-
-    private SerializablePredicate(Func<T, bool> predicate, Expression<Func<T, bool>> expression)
+    private SerializablePredicate(string Text, FrozenSet<string> namespaces, FrozenSet<Assembly> references)
     {
-        AsPredicate = predicate;
-        AsExpression = expression;
+        this.AsText = Text;
+        this.namespaces = namespaces;
+        this.references = references;
+        AsPredicate = CSharpScript.EvaluateAsync<Func<T, bool>>(
+            AsText,
+            ScriptOptions.Default
+                .AddReferences(references)
+                .AddImports(namespaces)
+        ).Result;
     }
-    public SerializablePredicate(Expression<Func<T, bool>> expression)
+    public SerializablePredicate(string AsText, string[] namespaces, Assembly[] references)
     {
-        AsPredicate = expression.Compile();
-        AsExpression = expression;
+        this.AsText = AsText;
+        AsPredicate = CSharpScript.EvaluateAsync<Func<T, bool>>(
+            AsText,
+            ScriptOptions.Default
+                .AddReferences(references)
+                .AddImports(namespaces)
+        ).Result;
+
+        this.namespaces = namespaces.ToFrozenSet();
+        this.references = references.ToFrozenSet();
+    }
+    public SerializablePredicate(Expression<Func<T, bool>> AsText, string[] namespaces, Assembly[] references)
+    {
+        this.AsText = AsText.ToString();
+        AsPredicate = AsText.Compile();
+
+        this.namespaces = namespaces.ToFrozenSet();
+        this.references = references.ToFrozenSet();
     }
 
-    public override string ToString()
+    public string Serialize()
     {
-        return AsExpression.ToString();
+        var sb = new StringBuilder(AsText).Append(SERIALIZATION_SECTION_SEPARATOR);
+        foreach (var str in namespaces)
+        {
+            sb.Append(str).Append(SERIALIZATION_SEPARATOR);
+        }
+        sb.Append(SERIALIZATION_SECTION_SEPARATOR);
+        foreach (var str in references)
+        {
+            sb.Append(str.FullName).Append(SERIALIZATION_SEPARATOR);
+        }
+
+        return sb.ToString();
+    }
+    public override string? ToString() => AsText;
+    public static SerializablePredicate<T> Parse(string serializedPredicate)
+    {
+        Span<string> sections = serializedPredicate.Split(SERIALIZATION_SECTION_SEPARATOR);
+
+        string Text = sections[0];
+
+        FrozenSet<string> namespaces = sections[1].Split(SERIALIZATION_SEPARATOR).Where(o => !o.IsWhiteSpace()).ToFrozenSet();
+        FrozenSet<Assembly> references = sections[2].Split(SERIALIZATION_SEPARATOR).Where(o => !o.IsWhiteSpace()).Select(o => Assembly.Load(o)).ToFrozenSet();
+
+        return new SerializablePredicate<T>(Text, namespaces, references);
     }
 
     public int CompareTo(object? obj)
     {
-        return 1;
+        return AsText.CompareTo(obj);
     }
-
-    public static SerializablePredicate<T> Parse(string serializedPredicate)
-    {
-        var exp = DynamicExpressionParser.ParseLambda<T, bool>(CONFIG, false, serializedPredicate);
-        return new SerializablePredicate<T>(exp.Compile(), exp);
-    }
-
-    public static implicit operator Func<T, bool>(SerializablePredicate<T> serPred) => serPred.AsPredicate;
-    public static implicit operator Expression<Func<T, bool>>(SerializablePredicate<T> serPred) => serPred.AsExpression;
 }
